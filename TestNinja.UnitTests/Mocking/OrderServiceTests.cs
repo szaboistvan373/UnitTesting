1d51a28851142b41e19197010c90f03281178a7a
Istvan Szabo <ripya420@gmail.com>
2019-05-26 17:23:09 +0200

    Add tests for product and order service classes

---
+++ TestNinja.UnitTests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking {
+    [TestFixture]
+    class OrderServiceTests {
+        [Test]
+        public void PlaceOrder_WhenCalled_StoreOrder() {
+            var storage = new Mock<IStorage>();
+            var orderService = new OrderService(storage.Object);
+
+            var newOrder = new Order();
+
+            orderService.PlaceOrder(newOrder);
+
+            storage.Verify(s => s.Store(newOrder));
+        }
+    }
+}
---
+++ TestNinja.UnitTests/Mocking/ProductTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking {
+    [TestFixture]
+    class ProductTests {
+        [Test]
+        public void GetPrice_GoldCustomer_Return30PercentDiscounterPrice() {
+            var product = new Product {
+                ListPrice = 10
+            };
+
+            var result = product.GetPrice(new Customer { IsGold = true });
+
+            Assert.That(result, Is.EqualTo(7));
+        }
+
+        [Test]
+        public void GetPrice_NormalCustomer_ReturnOriginalPrice() {
+            var product = new Product {
+                ListPrice = 10
+            };
+
+            var result = product.GetPrice(new Customer { IsGold = false });
+
+            Assert.That(result, Is.EqualTo(10));
+        }
+    }
+}
