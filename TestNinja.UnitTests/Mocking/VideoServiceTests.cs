f29443c4e49ca7054118166232616622d81b996d
Istvan Szabo <ripya420@gmail.com>
2019-05-26 16:58:35 +0200

    Add moq framework to project + add tests for video service

---
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+ 
+namespace TestNinja.UnitTests.Mocking {
+    [TestFixture]
+    class VideoServiceTests {
+        private VideoService _videoService;
+        private Mock<IFileReader> _fileReader;
+
+        [SetUp]
+        public void SetUp() {
+            _fileReader = new Mock<IFileReader>();
+            _videoService = new VideoService(_fileReader.Object);
+        }
+
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnErrorMessage() {
+            _fileReader.Setup(fr => fr.Read("video.txt"))
+                .Returns("");
+
+            var result = _videoService.ReadVideoTitle();
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+
+        [Test]
+        [TestCase("{title:'Avengers: Infinity War',id:12,isProcessed:true}", "Avengers: Infinity War")]
+        [TestCase("{title:'Avengers: Endgame',id:631,isProcessed:false}", "Avengers: Endgame")]
+        public void ReadVideoTitle_FileWitHJson_ReturnTitle(string videoJson, string title) {
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns(videoJson);
+
+            var result = _videoService.ReadVideoTitle();
+
+            Assert.That(result, Is.EqualTo(title));
+        }
+    }
+}
--- TestNinja/Mocking/VideoService.cs
+++ TestNinja/Mocking/VideoService.cs
@@ -1,58 +1,62 @@
-﻿using System;
+﻿using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.IO;
 using System.Linq;
-using Newtonsoft.Json;
 
-namespace TestNinja.Mocking
-{
-    public class VideoService
-    {
-        private IFileReader _fileReader;
+namespace TestNinja.Mocking {
+    public interface IFileReader {
+        string Read(string filePath);
+    }
 
-        public VideoService(IFileReader fileReader = null)
-        {
-            _fileReader = fileReader ?? new FileReader();
-        }
-
-        public string ReadVideoTitle()
-        {
-            var str = _fileReader.Read("video.txt");
-            var video = JsonConvert.DeserializeObject<Video>(str);
-            if (video == null)
-                return "Error parsing the video.";
-            return video.Title;
-        }
-
-        public string GetUnprocessedVideosAsCsv()
-        {
-            var videoIds = new List<int>();
-            
-            using (var context = new VideoContext())
-            {
-                var videos = 
-                    (from video in context.Videos
-                    where !video.IsProcessed
-                    select video).ToList();
-                
-                foreach (var v in videos)
-                    videoIds.Add(v.Id);
-
-                return String.Join(",", videoIds);
-            }
+    public class FileReader : IFileReader {
+        public string Read(string filePath) {
+            return File.ReadAllText(filePath);
         }
     }
 
-    public class Video
-    {
+    public class VideoService {
+        private readonly IFileReader _fileReader;
+
+        public VideoService(IFileReader fileReader = null) {
+            _fileReader = fileReader ?? new FileReader();
+        }
+
+        public string ReadVideoTitle() {
+            var str = _fileReader.Read("video.txt");
+            var video = JsonConvert.DeserializeObject<Video>(str);
+
+            if (video == null)
+                return "Error parsing the video.";
+
+            return video.Title;
+        }
+
+        //public string GetUnprocessedVideosAsCsv() {
+        //    var videoIds = new List<int>();
+
+        //    using (var context = new VideoContext()) {
+        //        var videos =
+        //            (from video in context.Videos
+        //             where !video.IsProcessed
+        //             select video).ToList();
+
+        //        foreach (var v in videos) {
+        //            videoIds.Add(v.Id);
+        //        }
+
+        //        return string.Join(",", videoIds);
+        //    }
+        //}
+    }
+
+    public class Video {
         public int Id { get; set; }
         public string Title { get; set; }
         public bool IsProcessed { get; set; }
     }
 
-    public class VideoContext : DbContext
-    {
+    public class VideoContext : DbContext {
         public DbSet<Video> Videos { get; set; }
     }
 }
