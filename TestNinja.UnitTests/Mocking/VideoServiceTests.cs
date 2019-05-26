7b8d116f4b39cd46aec412eb831d529dff88e302
Istvan Szabo <ripya420@gmail.com>
2019-05-26 18:56:09 +0200

    Refactor + add unit tests for GetUnprocessedVideos method

--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -1,22 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Moq;
 using NUnit.Framework;
 using TestNinja.Mocking;
- 
+
 namespace TestNinja.UnitTests.Mocking {
     [TestFixture]
     class VideoServiceTests {
         private VideoService _videoService;
         private Mock<IFileReader> _fileReader;
+        private Mock<IVideoRepository> _videoRepository;
 
         [SetUp]
         public void SetUp() {
             _fileReader = new Mock<IFileReader>();
-            _videoService = new VideoService(_fileReader.Object);
+            _videoRepository = new Mock<IVideoRepository>();
+
+            _videoService = new VideoService(_fileReader.Object, _videoRepository.Object);
         }
 
         [Test]
@@ -30,14 +29,37 @@ namespace TestNinja.UnitTests.Mocking {
         }
 
         [Test]
-        [TestCase("{title:'Avengers: Infinity War',id:12,isProcessed:true}", "Avengers: Infinity War")]
-        [TestCase("{title:'Avengers: Endgame',id:631,isProcessed:false}", "Avengers: Endgame")]
+        [TestCase("{title:'Avengers: Infinity War',id:1,isProcessed:true}", "Avengers: Infinity War")]
+        [TestCase("{title:'Avengers: Endgame',id:3,isProcessed:false}", "Avengers: Endgame")]
         public void ReadVideoTitle_FileWitHJson_ReturnTitle(string videoJson, string title) {
             _fileReader.Setup(fr => fr.Read("video.txt")).Returns(videoJson);
 
             var result = _videoService.ReadVideoTitle();
 
             Assert.That(result, Is.EqualTo(title));
+        }
+
+        [Test]
+        public void GetUnprocessedVideosAsCsv_AllVideosAreProcessed_ReturnAnEmptyString() {
+            _videoRepository.Setup(vr => vr.GetUnprocessedVideos()).Returns(new List<Video>());
+
+            var result = _videoService.GetUnprocessedVideosAsCsv();
+
+            Assert.That(result, Is.EqualTo(""));
+        }
+
+        [Test]
+        public void GetUnprocessedVideosAsCsv_ManyUnprocessedVideos_ReturnVideoIdsAsString() {
+            _videoRepository.Setup(vr => vr.GetUnprocessedVideos()).Returns(new List<Video>() {
+                new Video { Id = 9 },
+                new Video { Id = 5 },
+                new Video { Id = 10 },
+                new Video { Id = 25 },
+            });
+
+            var result = _videoService.GetUnprocessedVideosAsCsv();
+
+            Assert.That(result, Is.EqualTo("9,5,10,25"));
         }
     }
 }
--- TestNinja/Mocking/VideoService.cs
+++ TestNinja/Mocking/VideoService.cs
@@ -17,9 +17,11 @@ namespace TestNinja.Mocking {
 
     public class VideoService {
         private readonly IFileReader _fileReader;
+        private IVideoRepository _videoRepository;
 
-        public VideoService(IFileReader fileReader = null) {
+        public VideoService(IFileReader fileReader = null, IVideoRepository videoRepository = null) {
             _fileReader = fileReader ?? new FileReader();
+            _videoRepository = videoRepository ?? new VideoRepository();
         }
 
         public string ReadVideoTitle() {
@@ -32,22 +34,34 @@ namespace TestNinja.Mocking {
             return video.Title;
         }
 
-        //public string GetUnprocessedVideosAsCsv() {
-        //    var videoIds = new List<int>();
+        public string GetUnprocessedVideosAsCsv() {
+            var videoIds = new List<int>();
 
-        //    using (var context = new VideoContext()) {
-        //        var videos =
-        //            (from video in context.Videos
-        //             where !video.IsProcessed
-        //             select video).ToList();
+            var videos = _videoRepository.GetUnprocessedVideos();
 
-        //        foreach (var v in videos) {
-        //            videoIds.Add(v.Id);
-        //        }
+            foreach (var v in videos) {
+                videoIds.Add(v.Id);
+            }
 
-        //        return string.Join(",", videoIds);
-        //    }
-        //}
+            return string.Join(",", videoIds);
+        }
+    }
+
+    public interface IVideoRepository {
+        IEnumerable<Video> GetUnprocessedVideos();
+    }
+
+    class VideoRepository : IVideoRepository {
+        public IEnumerable<Video> GetUnprocessedVideos() {
+            using (var context = new VideoContext()) {
+                var videos =
+                    (from video in context.Videos
+                     where !video.IsProcessed
+                     select video).ToList();
+
+                return videos;
+            }
+        }
     }
 
     public class Video {
