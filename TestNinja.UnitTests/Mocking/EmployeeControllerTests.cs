00bd3c7a2bb53c758c31119c4362ac8bb99c96d6
Istvan Szabo <ripya420@gmail.com>
2019-05-26 20:42:02 +0200

    Refactor + add tests for EmoloyeeController class

---
+++ TestNinja.UnitTests/Mocking/EmployeeControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking {
+    [TestFixture]
+    class EmployeeControllerTests {
+        private EmployeeController _employeeController;
+        private Mock<IEmployeeStorage> _storage;
+
+        [SetUp]
+        public void SetUp() {
+            _storage = new Mock<IEmployeeStorage>();
+            _employeeController = new EmployeeController {
+                EmployeeStorage = _storage.Object
+            };
+        }
+
+        [Test]
+        public void DeleteEmployee_WhenCalled_DeleteThenEmployeeFromDb() {
+            _employeeController.DeleteEmployee(10);
+
+            _storage.Verify(s => s.DeleteEmployee(10));
+        }
+
+        [Test]
+        public void DeleteEmployee_WhenCalled_ReturnRedirectToEmployeesAction() {
+            var result = _employeeController.DeleteEmployee(10);
+
+            Assert.That(result, Is.TypeOf<RedirectResult>());
+        }
+    }
+}
--- TestNinja/Mocking/EmployeeController.cs
+++ TestNinja/Mocking/EmployeeController.cs
@@ -1,44 +1,55 @@
 ﻿using System.Data.Entity;
 
-namespace TestNinja.Mocking
-{
-    public class EmployeeController
-    {
-        private EmployeeContext _db;
+namespace TestNinja.Mocking {
+    public class EmployeeController {
+        public IEmployeeStorage EmployeeStorage { private get; set; }
 
-        public EmployeeController()
-        {
-            _db = new EmployeeContext();
+        public EmployeeController() {
+            EmployeeStorage = new EmployeeStorage();
         }
 
-        public ActionResult DeleteEmployee(int id)
-        {
-            var employee = _db.Employees.Find(id);
-            _db.Employees.Remove(employee);
-            _db.SaveChanges();
+        public ActionResult DeleteEmployee(int id) {
+            EmployeeStorage.DeleteEmployee(id);
+
             return RedirectToAction("Employees");
         }
 
-        private ActionResult RedirectToAction(string employees)
-        {
+        private ActionResult RedirectToAction(string employees) {
             return new RedirectResult();
         }
     }
 
     public class ActionResult { }
- 
+
     public class RedirectResult : ActionResult { }
-    
-    public class EmployeeContext
-    {
+
+    public class EmployeeContext {
         public DbSet<Employee> Employees { get; set; }
 
-        public void SaveChanges()
-        {
+        public void SaveChanges() {
         }
     }
 
-    public class Employee
-    {
+    public interface IEmployeeStorage {
+        void DeleteEmployee(int employeeId);
+    }
+
+    public class EmployeeStorage : IEmployeeStorage {
+        private EmployeeContext _db;
+
+        public EmployeeStorage() {
+            _db = new EmployeeContext();
+        }
+
+        public void DeleteEmployee(int employeeId) {
+            var employee = _db.Employees.Find(employeeId);
+            if (employee == null)
+                return;
+            _db.Employees.Remove(employee);
+            _db.SaveChanges();
+        }
+    }
+
+    public class Employee {
     }
 }
